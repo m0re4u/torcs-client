f431fd5ee8d8c0e93d2a5f579763b2c6750d1d0d
Martin van Harmelen <Martin@vanharmelen.com>
2017-09-07 12:42:33 +0200

    pep8 fix

--- pytocl/main.py
+++ pytocl/main.py
@@ -7,11 +7,21 @@ from pytocl.protocol import Client
 
 def main():
     """Main entry point of application."""
-    parser = argparse.ArgumentParser(description='Client for TORCS racing car simulation with SCRC '
-                                                 'network server.')
-    parser.add_argument('--hostname', help='Racing server host name.', default='localhost')
-    parser.add_argument('--port', help='Port to connect, 3001 - 3010 for clients 1 - 10.',
-                        type=int, default=3001)
+    parser = argparse.ArgumentParser(
+        description='Client for TORCS racing car simulation with SCRC network'
+                    ' server.'
+    )
+    parser.add_argument(
+        '--hostname',
+        help='Racing server host name.',
+        default='localhost'
+    )
+    parser.add_argument(
+        '--port',
+        help='Port to connect, 3001 - 3010 for clients 1 - 10.',
+        type=int,
+        default=3001
+    )
     parser.add_argument('-v', help='Debug log level.', action='store_true')
     args = parser.parse_args()
 
@@ -21,7 +31,10 @@ def main():
     else:
         level = logging.INFO
     del args.v
-    logging.basicConfig(level=level, format="%(asctime)s %(levelname)7s %(name)s %(message)s")
+    logging.basicConfig(
+        level=level,
+        format="%(asctime)s %(levelname)7s %(name)s %(message)s"
+    )
 
     # start client loop:
     client = Client(**args.__dict__)
