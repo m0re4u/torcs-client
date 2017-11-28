Fixed normalization?

ANNDriver.py
@@ -101,9 +101,9 @@ class ANNDriver(Driver):
             if i == 1:
                 new_sensors.append(sensor)
 
-            # Angle to track -pi --> pi radians
+            # Angle to track - 1/2 pi --> 1/2 pi radians
             if i == 2:
-                new_sensors.append((sensor + np.pi) / (2 * np.pi))
+                new_sensors.append((sensor + (0.5 * np.pi)) / np.pi)
 
             # Track edges 0 --> 200 meters 
             if i > 2:

nn/dataset.py
@@ -20,7 +20,6 @@ class DriverDataset(Dataset):
         if normalize:
             self.normalize()
 
-
     def normalize(self):
         # Normalize data
         new_data = []
@@ -37,9 +36,9 @@ class DriverDataset(Dataset):
             if i == 4:
                 new_data.append(column)
 
-            # Angle to track -pi --> pi radians
+            # Angle to track -1/2 pi --> 1/2 pi radians
             if i == 5:
-                new_data.append([(angle + np.pi) / (2 * np.pi) for angle in column])
+                new_data.append([((angle + (0.5 * np.pi)) / np.pi) for angle in column])
 
             # Track edges 0 --> 200 meters
             if i > 5: